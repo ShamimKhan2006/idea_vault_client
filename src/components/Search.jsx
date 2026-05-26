e6b6e9992a7d5539ec6e6fd86bd96ea53b7d11f4
ShamimKhan2006 <mdshamim200616@gmail.com>
2026-05-26 22:10:53 +0600

    fix

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,43 +1,50 @@
-// "use client";
-// import { useRouter, useSearchParams } from "next/navigation";
-// import { useState } from "react";
-// import { Button, SearchField } from "@heroui/react";
-// import Category from "./Category";
+"use client";
 
-// const Search = () => {
-//   const router = useRouter();
-//   const searchParams = useSearchParams();
-//   const [search, setSearch] = useState(searchParams.get("search") || "");
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { Button, SearchField } from "@heroui/react";
+import Category from "./Category";
 
-//   const handle = (e) => {
-//     e.preventDefault();
-//     router.push(`/ideas?search=${search}`);
-//   };
+const Search = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-//   return (
-//     <div className="w-full mx-auto my-10">
-//       <form onSubmit={handle}>
-//         <div className="flex items-center gap-4">
-//           <SearchField.Input 
-//   placeholder="Search..."
-//   value={search} 
-//   onChange={(e) => setSearch(e.target.value)} /
-// />
-//             <SearchField.Group>
-//               <SearchField.SearchIcon />
-//               <SearchField.Input placeholder="Search..." />
-//               <SearchField.ClearButton />
-//             </SearchField.Group>
-         
+  const [search, setSearch] = useState(
+    searchParams.get("search") || ""
+  );
 
-//           <Button type="submit" className="bg-green-500 text-white">
-//             Search
-//           </Button>
-//           <Category />
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
+  const handle = (e) => {
+    e.preventDefault();
+    router.push(`/ideas?search=${search}`);
+  };
 
-// export default Search;
+  return (
+    <div className="w-full mx-auto my-10">
+      <form onSubmit={handle}>
+        <div className="flex items-center gap-4">
+
+          <SearchField.Group>
+            <SearchField.SearchIcon />
+            <SearchField.Input
+              placeholder="Search..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <SearchField.ClearButton
+              onClick={() => setSearch("")}
+            />
+          </SearchField.Group>
+
+          <Button type="submit" className="bg-green-500 text-white">
+            Search
+          </Button>
+
+          <Category />
+
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
