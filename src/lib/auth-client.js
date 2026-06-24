ec4402a3c40d50c145c8c1b8e1fbd4f799d94124
ShamimKhan2006 <mdshamim200616@gmail.com>
2026-06-24 22:04:20 +0600

    fix

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,7 +2,7 @@ import { jwtClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
 
-    baseURL:process.env.BETTER_AUTH_URL,
+    baseURL:"https://idea-vault-client-ifzm.vercel.app",
     plugins:[
         jwtClient()
     ]
