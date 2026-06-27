49e77a0c39ff59a70d62f361d05316a8a1f60ce5
ShamimKhan2006 <mdshamim200616@gmail.com>
2026-06-27 16:56:54 +0600

    project also error free

--- src/app/loading.js
+++ src/app/loading.js
@@ -2,10 +2,10 @@ export default function Loading() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="relative h-20 w-20">
-        <span className="absolute inset-0 rounded-full border-[5px] border-green-500 border-t-transparent animate-spin"></span>
+        <span className="absolute inset-0 rounded-full border-[5px] border-green-400 border-t-transparent animate-spin"></span>
 
         <span
-          className="absolute inset-3 rounded-full border-[5px] border-green-700 border-b-transparent animate-spin"
+          className="absolute inset-3 rounded-full border-[5px] border-green-300 border-b-transparent animate-spin"
           style={{
             animationDirection: "reverse",
             animationDuration: "1.2s",
