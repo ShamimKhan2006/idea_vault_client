72d5c976a3574d46208c6207634deb560da7c4bf
ShamimKhan2006 <mdshamim200616@gmail.com>
2026-06-27 22:44:14 +0600

    completed

--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -150,3 +150,4 @@ const Navber = () => {
 };
 
 export default Navber;
+
--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,7 +2,7 @@ import { jwtClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
 
-    //   baseURL:"https://idea-vault-client-ifzm.vercel.app",
+    //   baseURL:"https://localhost:3000",
           baseURL:"https://client-ten-eta-68.vercel.app",
     plugins:[
         jwtClient()
