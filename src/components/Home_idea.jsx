e92ce0f65a7cd4783e8f2d764ea6cec702bbcba1
ShamimKhan2006 <mdshamim200616@gmail.com>
2026-06-28 11:40:21 +0600

    some bug

--- src/components/Home_idea.jsx
+++ src/components/Home_idea.jsx
@@ -6,12 +6,7 @@ import Link from 'next/link';
 import React from 'react';
 
 const Home_idea = async() => {
-    // const res=await fetch(`${process.env.NEXT_PUBLIC_URL}/featured`,{
-    //     headers:{
-    //         "content-type":"application/json"
-    //     },
-    //     body:JSON.stringify()
-    // })
+   
 
     const res = await fetch(
   `${process.env.NEXT_PUBLIC_URL}/featured`,
